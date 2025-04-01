b49cbb0f05902c9e4c73f90be22562011615517c
Dominicus <100403377+Dominicus0830@users.noreply.github.com>
2025-04-01 20:04:27 +0800

    test

--- src/main/java/org/domi/entityDrops/minecraft/Sniffer.java
+++ src/main/java/org/domi/entityDrops/minecraft/Sniffer.java
@@ -7,7 +7,6 @@ import org.domi.init.itemlists.PMFItemList;
 
 public class Sniffer {
 
-
     @SubscribeEvent
     public void onLivingDrops(LivingDropsEvent event) {
         EntityDropUtil.entityDrop(event, "minecraft:sniffer", PMFItemList.RAW_SNIFFER_MEAT, 2, 1);
