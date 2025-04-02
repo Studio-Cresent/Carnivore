cb0120feb00fe795810eb52c92352274055276b1
Dominicus <100403377+Dominicus0830@users.noreply.github.com>
2025-04-02 19:46:41 +0800

    Test

--- src/main/java/org/domi/entityDrops/minecraft/Tadpole.java
+++ src/main/java/org/domi/entityDrops/minecraft/Tadpole.java
@@ -5,6 +5,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.domi.entityDrops.EntityDropUtil;
 import org.domi.init.itemlists.PMFItemList;
 public class Tadpole {
+
     @SubscribeEvent
     public void onLivingDrops(LivingDropsEvent event) {
         EntityDropUtil.entityDrop(event, "minecraft:tadpole", PMFItemList.TADPOLE, 1, 0);
